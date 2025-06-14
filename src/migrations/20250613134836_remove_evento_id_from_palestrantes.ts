d896401ec370a1073cd673e8959486db09452abc
Rafaelcolombo23 <99507920+Rafaelcolombo23@users.noreply.github.com>
2025-06-14 19:03:25 -0300

    ajustando ultimos detalhes

--- src/migrations/20250613134836_remove_evento_id_from_palestrantes.ts
+++ src/migrations/20250613134836_remove_evento_id_from_palestrantes.ts
@@ -2,8 +2,8 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.alterTable("palestrantes", (table) => {
-    table.dropForeign(["evento_id"]); // remove a foreign key
-    table.dropColumn("evento_id");    // remove a coluna
+    table.dropForeign(["evento_id"]); 
+    table.dropColumn("evento_id");   
   });
 }
 
